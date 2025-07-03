13fa0230ecfda8c0c1b27510b9d40d2bfd5aaac2
night-dreamer <charon.deadpan@gmail.com>
2025-07-03 21:28:25 +0800

    added database models and cloudinary config

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 
 import logger from "./libs/logger.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  logger.info(`Server is running at http://localhost:${port}`);
+connectDB().then(() => {
+  app.listen(port, () => {
+    logger.info(`Server is running at http://localhost:${port}`);
+  });
 });
